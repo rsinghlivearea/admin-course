3c9ae38aa7c3e212a1a725c2646452c0349da62e
Ravinder Singh <rsingh1@liveareacx.com>
2021-06-18 16:30:29 +0530

    little bit refactoring

--- react/bcProducts.tsx
+++ react/bcProducts.tsx
@@ -1,16 +1,19 @@
-import React, {FC} from 'react'
-import {Layout, PageBlock} from 'vtex.styleguide'
-import {useQuery} from 'react-apollo'
+import React, { FC } from 'react'
+import { Layout, PageBlock } from 'vtex.styleguide'
+import { useQuery } from 'react-apollo'
 
 import getBCProducts from './graphql/getBCProducts.graphql'
 
 const BCProducts: FC = () => {
-  console.log("test")
-  const {data} = useQuery(getBCProducts)
-  console.log("test")
+  const { data } = useQuery(getBCProducts)
+
   return (
     <Layout>
-      <PageBlock title="Titulo" subtitle="Alguma explicação." variation="full">
+      <PageBlock
+        title="Integration with Big Commerce Products API"
+        subtitle="Direct integration with BC Rest API https://developer.bigcommerce.com/api-reference/store-management/catalog/products/getproducts"
+        variation="full"
+      >
         <h1>BC Products!</h1>
         <p>{data?.getBCProducts}</p>
       </PageBlock>
